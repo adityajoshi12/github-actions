1e1f2ba6b6a898bdc6fd86fb8d820a7bbfa3b79b
Aditya <adjoshi@innominds.com>
2020-12-11 14:52:30 +0530

    actions added

---
+++ src/PersonList.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import React from "react";
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: [],
+  };
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
+      const persons = res.data;
+      this.setState({ persons });
+    });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.persons.map((person) => (
+          <li>{person.name}</li>
+        ))}
+      </ul>
+    );
+  }
+}
